aea90cb4b7b1479eafa37ae27d471bf47d590817
MrLoony <luni9393@gmail.com>
2024-04-30 17:26:32 +0400

    Add Player
    +1st person camera

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public float sensivityX;
+    public float sensivityY;
+
+    public Transform orientation;
+
+    private float xRotation;
+    private float yRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * sensivityX * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * sensivityY * Time.deltaTime;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // Camera rotation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
